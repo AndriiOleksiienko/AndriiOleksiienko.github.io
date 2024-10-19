c92e22ca7f49a721214851dc54aef45d73eff4eb
AndriiOleksiienko <andrii.oleksiienko-ki2101@kep.nung.edu.ua>
2024-10-19 10:18:56 +0300

    Add files via upload

--- LB5/script.js
+++ LB5/script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", getMyLocation)
 
+
 const ourCoords = {
     latitude: 48.943391454671854,
     longitude: 24.731924095723915
@@ -58,7 +59,7 @@ function computeDistance(startCoords, destCoords) {
     let startLongRads = degreesToRadians(startCoords.longitude);
     let destLatRads = degreesToRadians(destCoords.latitude);
     let destLongRads = degreesToRadians(destCoords.longitude);
-    let Radius = 6371; // вказаний радіус Землі у км
+    let Radius = 6371; 
 
     let distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) +
         Math.cos(startLatRads) * Math.cos(destLatRads) *
@@ -70,6 +71,44 @@ function computeDistance(startCoords, destCoords) {
 function degreesToRadians(degrees) {
     let radians = (degrees * Math.PI) / 180;
     return radians;
+}
+
+
+const map = L.map('map').setView([48.943391454671854, 24.731924095723915], 15);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+const marker = L.marker([48.943391454671854, 24.731924095723915]).addTo(map)
+    .bindPopup('Ваша поточна локація.')
+    .openPopup();
+
+marker.bindPopup('<h3>Ваше місце розташування</h3>').openPopup();
+
+function addCurrentLocationMarker() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            const lat = position.coords.latitude;
+            const lng = position.coords.longitude;
+            const timestamp = new Date(position.timestamp);
+
+            const marker = L.marker([lat, lng]).addTo(map)
+                .bindPopup(`<b>Ваше місце розташування</b><br>Координати: ${lat}, ${lng}<br>Час: ${timestamp.toLocaleTimeString()}`)
+                .openPopup();
+
+            map.setView([lat, lng], 13);
+        });
+    } else {
+        alert("Oops, no geolocation support");
+    }
+}
+
+function moveToLocation() {
+    const destLat = parseFloat(document.getElementById('destinationLat').value);
+    const destLng = parseFloat(document.getElementById('destinationLng').value);
+    map.setView([destLat, destLng], 15);
+
+    L.marker([destLat, destLng]).addTo(map)
+        .bindPopup('Пункт призначення.')
+        .openPopup();
 }
 
 
