3f7ef497a2667009f06f5a4ff7d9f8a86fa2da2e
AndriiOleksiienko <andrii.oleksiienko-ki2101@kep.nung.edu.ua>
2024-11-03 15:14:41 +0200

    Add files via upload

--- LB8/script.js
+++ LB8/script.js
@@ -1,37 +1,23 @@
-const breakpoints = [
-    { maxWidth: 450, slidesPerView: 1 },
-    { maxWidth: 650, slidesPerView: 2 },
-    { maxWidth: 850, slidesPerView: 3 },
-];
-
-
-
 var swiper = new Swiper(".swiper", {
     grabCursor: true,
     loop: true,
-    slidesPerView: calculateSlidesPerView(),
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        450: {
+            slidesPerView: 2
+        },
+        650: {
+            slidesPerView: 3
+        },
+        850: {
+            slidesPerView: 4
+        }
+    }
 });
 
 const button_left = document.getElementById("swipe-left").addEventListener("click", () => swiper.slidePrev(200));
 const button_right = document.getElementById("swipe-right").addEventListener("click", () => swiper.slideNext(200));
 
 
-function calculateSlidesPerView() {
-    let slides = 4; 
-
-    for (const breakpoint of breakpoints) {
-        if (window.innerWidth <= breakpoint.maxWidth) {
-            slides = breakpoint.slidesPerView;
-            break;
-        }
-    }
-    return slides;
-}
-
-function updateSlidesPerView() {
-    swiper.params.slidesPerView = calculateSlidesPerView();
-    swiper.update();
-}
-
-window.addEventListener("resize", updateSlidesPerView);
-updateSlidesPerView();
